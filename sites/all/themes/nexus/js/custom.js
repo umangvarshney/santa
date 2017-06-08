0fcf93b9a5febf144f7bf816c0248d0c2d263087
umangvar <umangvar@gmail.com>
2017-06-08 12:08:24 +0530

    santa

---
+++ sites/all/themes/nexus/js/custom.js
@@ -0,0 +1,58 @@
+jQuery(window).load(function() {
+    
+  /* Navigation */
+
+	jQuery('#main-menu > ul').superfish({ 
+		delay:       500,								// 0.1 second delay on mouseout 
+		animation:   { opacity:'show',height:'show'},	// fade-in and slide-down animation 
+		dropShadows: true								// disable drop shadows 
+	});	  
+
+	jQuery('#main-menu > ul').mobileMenu({
+		prependTo:'.mobilenavi'
+	});
+
+});
+
+
+( function() {
+	var is_webkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
+	    is_opera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
+	    is_ie     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
+
+	if ( ( is_webkit || is_opera || is_ie ) && 'undefined' !== typeof( document.getElementById ) ) {
+		var eventMethod = ( window.addEventListener ) ? 'addEventListener' : 'attachEvent';
+		window[ eventMethod ]( 'hashchange', function() {
+			var element = document.getElementById( location.hash.substring( 1 ) );
+
+			if ( element ) {
+				if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) )
+					element.tabIndex = -1;
+
+				element.focus();
+			}
+		}, false );
+	}
+})();
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
---
+++ sites/all/themes/nexus/templates/page--front.tpl.php
@@ -0,0 +1,224 @@
+<?php
+/**
+ * @file
+ * Default theme implementation to display a single Drupal page.
+ *
+ * Available variables:
+ *
+ * General utility variables:
+ * - $base_path: The base URL path of the Drupal installation. At the very
+ *   least, this will always default to /.
+ * - $directory: The directory the template is located in, e.g. modules/system
+ *   or themes/garland.
+ * - $is_front: TRUE if the current page is the front page.
+ * - $logged_in: TRUE if the user is registered and signed in.
+ * - $is_admin: TRUE if the user has permission to access administration pages.
+ *
+ * Site identity:
+ * - $front_page: The URL of the front page. Use this instead of $base_path,
+ *   when linking to the front page. This includes the language domain or
+ *   prefix.
+ * - $logo: The path to the logo image, as defined in theme configuration.
+ * - $site_name: The name of the site, empty when display has been disabled
+ *   in theme settings.
+ * - $site_slogan: The slogan of the site, empty when display has been disabled
+ *   in theme settings.
+ *
+ * Navigation:
+ * - $main_menu (array): An array containing the Main menu links for the
+ *   site, if they have been configured.
+ * - $secondary_menu (array): An array containing the Secondary menu links for
+ *   the site, if they have been configured.
+ * - $breadcrumb: The breadcrumb trail for the current page.
+ *
+ * Page content (in order of occurrence in the default page.tpl.php):
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title: The page title, for use in the actual HTML content.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ * - $messages: HTML for status and error messages. Should be displayed
+ *   prominently.
+ * - $tabs (array): Tabs linking to any sub-pages beneath the current page
+ *   (e.g., the view and edit tabs when displaying a node).
+ * - $action_links (array): Actions local to the page, such as 'Add menu' on the
+ *   menu administration interface.
+ * - $feed_icons: A string of all feed icons for the current page.
+ * - $node: The node object, if there is an automatically-loaded node
+ *   associated with the page, and the node ID is the second argument
+ *   in the page's path (e.g. node/12345 and node/12345/revisions, but not
+ *   comment/reply/12345).
+ *
+ * Regions:
+ * - $page['help']: Dynamic help text, mostly for admin pages.
+ * - $page['content']: The main content of the current page.
+ * - $page['sidebar_first']: Items for the first sidebar.
+ * - $page['sidebar_second']: Items for the second sidebar.
+ * - $page['header']: Items for the header region.
+ * - $page['footer']: Items for the footer region.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_page()
+ * @see template_process()
+ */
+?>
+<div class="front-wrapper">
+	<div class="cover-page">
+		<div class="cover-layer">
+			<div class="cover-content full-width">
+				<div class="inner-layout">
+					<h1 class="front-page-header">Post By Santa</h1>
+				</div>
+				<div class="small-text">
+					<p>Customized letters for those who believe</p>
+				</div>
+				<div class="inner-widget">
+					<ul>
+						<li><a href="#" id="clickme">Get Started</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php if($page['top_banner']):?>
+<div class="bannertop" id="top_banner">
+<?php print render($page['top_banner']);?>
+</div>
+<?php endif;?>
+<div id="page" class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+  <header id="masthead" class="site-header container" role="banner">
+    <div class="row">
+      <div id="logo" class="site-branding col-sm-5">
+        <?php if ($logo): ?><div id="site-logo"><a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        </a></div><?php endif; ?>
+        <h1 id="site-title">
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
+        </h1>
+      </div>
+      <div class="col-sm-7 mainmenu">
+        <div class="mobilenavi"></div>
+        <nav id="navigation" role="navigation">
+          <div id="main-menu">
+            <?php 
+              if (module_exists('i18n_menu')) {
+                $main_menu_tree = i18n_menu_translated_tree(variable_get('menu_main_links_source', 'main-menu'));
+              } else {
+                $main_menu_tree = menu_tree(variable_get('menu_main_links_source', 'main-menu'));
+              }
+              print drupal_render($main_menu_tree);
+            ?>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </header>
+
+
+  <?php if($page['main_slider']):?> 
+<div id="slidebox" class="flexslider">
+<?php print render($page['main_slider']);?>
+</div>
+<?php endif;?>
+
+  <?php if($page['preface_first'] || $page['preface_middle'] || $page['preface_last']) : ?>
+    <?php $preface_col = ( 12 / ( (bool) $page['preface_first'] + (bool) $page['preface_middle'] + (bool) $page['preface_last'] ) ); ?>
+    <div id="preface-area">
+      <div class="container">
+        <div class="row">
+          <?php if($page['preface_first']): ?><div class="preface-block col-sm-<?php print $preface_col; ?>">
+            <?php print render ($page['preface_first']); ?>
+          </div><?php endif; ?>
+          <?php if($page['preface_middle']): ?><div class="preface-block col-sm-<?php print $preface_col; ?>">
+            <?php print render ($page['preface_middle']); ?>
+          </div><?php endif; ?>
+          <?php if($page['preface_last']): ?><div class="preface-block col-sm-<?php print $preface_col; ?>">
+            <?php print render ($page['preface_last']); ?>
+          </div><?php endif; ?>
+        </div>
+      </div>
+    </div>
+  <?php endif; ?>
+
+  <?php if($page['header']) : ?>
+    <div id="header-block">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <?php print render($page['header']); ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  <?php endif; ?>
+
+    <div class="android-content mdl-layout__content">
+    <?php print render($page['content']); ?>
+    </div>
+
+  <?php if($page['footer']) : ?>
+    <div id="footer-block">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <?php print render($page['footer']); ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($page['footer_first'] || $page['footer_second'] || $page['footer_third'] || $page['footer_fourth']): ?>
+    <?php $footer_col = ( 12 / ( (bool) $page['footer_first'] + (bool) $page['footer_second'] + (bool) $page['footer_third'] + (bool) $page['footer_fourth'] ) ); ?>
+    <div id="bottom">
+      <div class="container">
+        <div class="row">
+          <?php if($page['footer_first']): ?><div class="footer-block col-sm-<?php print $footer_col; ?>">
+            <?php print render ($page['footer_first']); ?>
+          </div><?php endif; ?>
+          <?php if($page['footer_second']): ?><div class="footer-block col-sm-<?php print $footer_col; ?>">
+            <?php print render ($page['footer_second']); ?>
+          </div><?php endif; ?>
+          <?php if($page['footer_third']): ?><div class="footer-block col-sm-<?php print $footer_col; ?>">
+            <?php print render ($page['footer_third']); ?>
+          </div><?php endif; ?>
+          <?php if($page['footer_fourth']): ?><div class="footer-block col-sm-<?php print $footer_col; ?>">
+            <?php print render ($page['footer_fourth']); ?>
+          </div><?php endif; ?>
+        </div>
+      </div>
+    </div>
+  <?php endif; ?>
+
+  <footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="container">
+      <div class="row">
+        <div class="fcred col-sm-12">
+          <?php print t('Copyright'); ?> &copy; <?php echo date("Y"); ?>, Santa.com. 
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<script type="text/javascript">
+jQuery(document).init(function(){
+	if(getCookie('front') == "" || getCookie('front') == 'undefined') {
+		setCookie('front',1,1);
+		jQuery('#page').css({'display':'none'});
+	}
+	else {
+		 jQuery( ".front-wrapper" ).css({'display':'none'});
+		}
+});
+jQuery( "#clickme" ).click(function() {
+	jQuery( ".front-wrapper" ).slideUp( "slow", function() {
+	    // Animation complete.
+	    jQuery( ".front-wrapper" ).css({'display':'none'});
+		jQuery('#page').css({'display':'block'});
+		location.reload();
+	  });
+	});
+</script>
