540e6e0964a2cf00093cbd5b3fc191dfd322bb41
umangvar <umangvar@gmail.com>
2017-06-16 17:38:27 +0530

    Version 1.3

---
+++ conn.php
@@ -0,0 +1,6 @@
+<?php 
+$con = mysqli_connect('10.193.68.94:82', 'root','');
+var_dump($con);
+///$db = mysql_select_db('santa',$con);
+//var_dump($db);
+?>
--- sites/all/modules/custom/preview/preview.js
+++ sites/all/modules/custom/preview/preview.js
@@ -1,33 +1,36 @@
+/****
+ * Preview JS for preview letter
+ * @copyright PostBy Santa
+ * @param $
+ */
 (function($) {
   Drupal.behaviors.preview = {
     attach: function (context, settings) {
-    	jQuery('img.lazy').lazyload({effect : "fadeIn"});
-    	var data = jQuery.parseJSON(settings.preview.json);
+    	$('img.lazy').lazyload({effect : "fadeIn"});
+    	var data = $.parseJSON(settings.preview.json);
     	var textdata = data.text;
     	var formid = data.cartformid;
     	formid = formid.replace(/_/g,'-');
+    	var terms = data.term;
     	var flag = false;
     	var fieldsArray = requiredFieldArray(formid);
     	var Fields = fieldsArray[0];
     	var requiredFields = fieldsArray[1];
-    	var tag = ["#{firstname}#","#{lastname}#"];
-    	/*var Fields = ['edit-line-item-fields-field-cfname-und-0-value','edit-line-item-fields-field-clname-und-0-value'];
-    	var requriedFields = ['edit-line-item-fields-field-cfname-und-0-value','edit-line-item-fields-field-clname-und-0-value'];*/
-    	
-    	jQuery("form#"+formid+ " input[type=text]").on('keyup',function(){
+    	var tag = $.map(terms, function(value, index) {
+    	    return [value];
+    	});
+    	$("form#"+formid+ " input[type=text]").on('keyup',function(){
     		//console.log("working");
     		var valid = validateForm(requiredFields);
-    		var text = str_replace(tag, Fields, textdata);
     		if(valid && !flag) {
     			flag = true;
-    			jQuery('.preview_container #text-section').html(text);
-    			jQuery('.preview_container #back-section').html('<img  src="'+data.imgpath+'" width="100%" height="100%"/>');
+    		    text = str_replace(tag, Fields, textdata);
+    			$('.preview_container #text-section').html(text);
+    			$('.preview_container #back-section').html('<img  src="'+data.imgpath+'" width="100%" height="100%"/>');
     		}
     		if(flag) {
-    			var id = jQuery(this).attr('id');
-    			var value = jQuery(this).val();
-    			 text = updateText(textdata,value,tag[1]);
-    			 jQuery('.preview_container #text-section').html(text);
+    			 text = str_replace(tag, Fields, textdata);
+    			 $('.preview_container #text-section').html(text);
     		}
     	});
     }
@@ -46,14 +49,14 @@
   function str_replace(search, replace, subject) {
 	  var count = 0;
 	    for (i=0; i<replace.length; i++) {
-	    	var value = jQuery('#'+replace[i]).val();
+	    	var value = $('#'+replace[i]).val();
 	        subject = subject.replace(search[i], value);
 	    }
 	    return subject;
  }
   function validateForm(requriedFields) {
 	  var isValid = true;
-	  jQuery.each(requriedFields,function(i,val) {
+	  $.each(requriedFields,function(i,val) {
 	    if ( $('#'+val).val() === '' )
 	        isValid = false;
 	  });
@@ -63,10 +66,10 @@
 	  var requiredFields = [];
 	  var data = [];
 	  var Fields = [];
-	  jQuery("form#"+formid+ " input[type=text]").each(function() {
-		  Fields.push(jQuery(this).attr('id'));
-		  if( jQuery(this).hasClass('required')) {
-			  requiredFields.push(jQuery(this).attr('id'));
+	  $("form#"+formid+ " input[type=text]").each(function() {
+		  Fields.push($(this).attr('id'));
+		  if( $(this).hasClass('required')) {
+			  requiredFields.push($(this).attr('id'));
 		  }  
 	});
 	  data[0] =Fields;
